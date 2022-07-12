55b8d4b23000024dd376855e5848e6d722874f70
Okeoma Nwabueze <okeomanwabueze@trentu.ca>
2022-07-11 19:01:27 -0600

    original

---
+++ src/Card.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Card = () => {
+    return(
+        <div>
+            <img alt='robots' src=''/>
+            <div>
+                <h2>Billy Joe</h2>
+                <p>billyjoe@gmail.com</p>
+            </div>
+        </div>
+    )
+}
+
+export default Card;
--- src/index.js
+++ src/index.js
@@ -1,22 +1,36 @@
-import React from 'react'; // react is a view library
-import ReactDOM from 'react-dom/client';
-import './index.css';// ./ means it is in the same directory
-//import App from './App';
+// import React from 'react'; // react is a view library
+// import ReactDOM from 'react-dom/client'; is for the webpage rendering
+// import './index.css';// ./ means it is in the same directory
+// //import App from './App';
+// import reportWebVitals from './reportWebVitals';
+// import Hello from './Hello'; // created a hello component
+// import 'tachyons'; tachyons is a css package
+
+
+//  const root = ReactDOM.createRoot(document.getElementById('root'));
+//  root.render(
+//   <Hello greeting={'Hello' + ' React Ninja'} /> // I can add properties such as greeting
+//  )
+// //   <React.StrictMode>
+// //     <App />
+// //   </React.StrictMode>
+// // );
+
+// // If you want to start measuring performance in your app, pass a function
+// // to log results (for example: reportWebVitals(console.log))
+// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
+
+import React from 'react';
+import ReactDOM  from 'react-dom/client';
+import './index.css';
+import Card from './Card';
 import reportWebVitals from './reportWebVitals';
-import Hello from './Hello'; // created a hello component
 import 'tachyons';
 
+const root = ReactDOM.createRoot(document.getElementById('root'));
 
- const root = ReactDOM.createRoot(document.getElementById('root'));
- root.render(
-  <Hello greeting={'Hello' + ' React Ninja'} /> // I can add properties such as greeting
- )
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+root.render(
+    <Card />
+)
 reportWebVitals();
