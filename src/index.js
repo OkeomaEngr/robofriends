b42c65466aad538dad9dd0a4bc4d11ffc0017662
Okeoma Nwabueze <okeomanwabueze@trentu.ca>
2022-07-11 18:37:16 -0600

    robofriends test

--- src/index.js
+++ src/index.js
@@ -1,15 +1,20 @@
-import React from 'react';
+import React from 'react'; // react is a view library
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import './index.css';// ./ means it is in the same directory
+//import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Hello from './Hello'; // created a hello component
+import 'tachyons';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+
+ const root = ReactDOM.createRoot(document.getElementById('root'));
+ root.render(
+  <Hello greeting={'Hello' + ' React Ninja'} /> // I can add properties such as greeting
+ )
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
